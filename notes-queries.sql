da472fdc8721678084b05ff2ebe5bca7bef7146f
David Graves <davidgraves831@gmail.com>
2018-08-06 16:35:35 -0500

    finished day 1

---
+++ notes-queries.sql
@@ -0,0 +1,25 @@
+SELECT * FROM notes;
+
+-- SELECT * FROM notes
+-- LIMIT 5;
+
+-- SELECT * FROM notes
+-- ORDER BY title DESC;
+
+-- SELECT *  FROM notes
+-- WHERE title = 'Why you should forget everything you learned about cats';
+
+-- SELECT *  FROM notes
+-- WHERE title LIKE '%gaga%';
+
+-- UPDATE notes
+--   SET title = 'This is a  new title', content = 'This is the new content'
+--   WHERE id = '6';
+
+-- INSERT INTO notes (
+--     title, content) VALUES
+--     ('New Note test', 'the content to the new note test' );
+
+
+
+-- DELETE FROM notes WHERE id = 11;
